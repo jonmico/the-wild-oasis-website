e29511535794850a79d387eb30e7f519a4a475f5
Jon Mico <jonmico@gmail.com>
2024-06-06 15:37:43 -0400

    Get app up to speed on laptop.

---
+++ app/account/page.jsx
@@ -0,0 +1,7 @@
+export default function AccountPage() {
+  return (
+    <div>
+      <h1>This is the account page.</h1>
+    </div>
+  );
+}
---
+++ app/cabins/page.jsx
@@ -0,0 +1,7 @@
+export default function CabinsPage() {
+  return (
+    <div>
+      <h1>This is the cabins page</h1>
+    </div>
+  );
+}
