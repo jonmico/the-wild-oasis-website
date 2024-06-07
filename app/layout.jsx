f5086eed9bbc190bde7eda4d5ae28047660a67f2
Jon Mico <jonmico@gmail.com>
2024-06-07 10:18:56 -0400

    Layout + fix Tailwind config.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,3 @@
-import Logo from './_components/logo';
-import Navigation from './_components/navigation';
-
 import { Josefin_Sans } from 'next/font/google';
 
 const josefin = Josefin_Sans({
@@ -9,6 +6,7 @@ const josefin = Josefin_Sans({
 });
 
 import '@/app/_styles/globals.css';
+import Header from './_components/header';
 
 export const metadata = {
   title: {
@@ -23,14 +21,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className={'flex-1 px-8 py-12'}>
+          <main className='max-w-7xl mx-auto'>{children}</main>
+        </div>
       </body>
     </html>
   );
